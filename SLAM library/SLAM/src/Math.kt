3492ac79800caae5075b560e0aacbb55716d31ee
Jonatan Wästlund <jonte.w1@gmail.com>
2019-01-23 20:10:42 +0100

    Kanske fungerande SLAM kod
    Har skrivit en kod som identifierar vägger men den matchar inte väggar med de väggar man redan känner igen än.

--- SLAM library/SLAM/src/Math.kt
+++ SLAM library/SLAM/src/Math.kt
@@ -2,8 +2,55 @@ import kotlin.math.pow
 
 class Math{
 
-    fun lineSplitter(){
+    val threshHold = 1f
 
+    fun lineSplitter(dataSet : Array<Coordinate>) : Array<Wall>{
+        var done = false
+        var counter = -1
+
+        var wallCashe : Array<Wall>? = null
+        var walls : Array<Wall>? = null
+        var pointHightCashe : Array<Float>? = null
+
+        wallCashe!!.plus(Wall(dataSet))
+
+        while(counter<wallCashe.size){
+            counter += 1
+            if ((wallCashe[counter].direction!! <= 1f) and (wallCashe[counter].direction!! >= -1f)){
+                var wall = wallCashe[counter]
+                for (point in wall.points) {
+                    var difference = point.x - wall.first.x
+                    var pointCashe = Coordinate(point.x- difference, point.y - difference*wall.direction!!)
+                    pointHightCashe!!.plus((pointCashe.y-wall.first.y).pow(2))
+                }
+                var highestValue : Float = 0f
+                var highestPosition : Int = 0
+                for (value in pointHightCashe!!){
+                    if (value > highestValue){
+                        highestValue = value
+                        highestPosition = counter
+                    }
+                    counter += 1
+                }
+                if (highestValue > threshHold){
+                    var wallOne : Array<Coordinate>? = null
+                    var wallTwo : Array<Coordinate>? = null
+                    for (i in 0 until pointHightCashe.size ) {
+                        if(i <= threshHold){
+                            wallOne!!.plus(dataSet[i])
+                        }; if(i>= threshHold) {
+                            wallTwo!!.plus(dataSet[i])
+                        }
+                    }
+                    wallCashe.plus(Wall(wallOne!!))
+                    wallCashe.plus(Wall(wallTwo!!))
+                } else {
+                    walls!!.plus(wall)
+                }
+            }
+        }
+
+        return walls!!
     }
 
     /*fun linReg(dataSet : Array<Coordinate>) : Vector{
@@ -36,29 +83,22 @@ class Math{
         return Vector(k,l,m)
 
     }*/
-
-    fun furthestPoint(dataSet : Array<Double>, line : Array<Double>){
-
-    }
 }
 
-class Coordinate(xCord : Float, yCord: Float){
+data class Coordinate(val x : Float,val y : Float){
 
-    var x : Float = 0f
-    var y : Float = 0f
-
-    init{
-        this.x = xCord
-        this.y = yCord
-    }
 }
 
-class spaceWall(firstIn : Coordinate, secondIn : Coordinate){
-    var first : Coordinate? = null
-    var second : Coordinate? = null
+data class Wall(val points : Array<Coordinate>){
+
+    val first = points.first()
+    val last = points.last()
+
+    var direction : Float? = null
 
     init {
-        this.first = firstIn
-        this.second = secondIn
+        if (last.x-first.x != 0f) {
+            val direction = (last.y-first.y)/(last.x-first.x)
+        }
     }
 }
